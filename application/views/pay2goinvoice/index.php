36f9e9efea699449370bb1b24d67b8032bde827c
Allen Lu <iniquity.spirit@gmail.com>
2018-05-17 15:31:36 +0800

    Changes to be committed: 	modified:   application/views/pay2goinvoice/index.php
    把路徑中的pay2goinvoice 改成 Pay2GoInvoice (case sensitive)

--- application/views/pay2goinvoice/index.php
+++ application/views/pay2goinvoice/index.php
@@ -6,27 +6,27 @@
         <div class="w3-container">
 
             <div class="w3-panel w3-border w3-border-red w3-hover-border-green">
-                <p><?php echo anchor('pay2goinvoice/index/issue_invoice', '建立發票', '建立發票') ?></p>
+                <p><?php echo anchor('Pay2GoInvoice/index/issue_invoice', '建立發票', '建立發票') ?></p>
                 <!--<p><a href="index.php/pay2goinvoice/index/issue_invoice">建立發票</a></p>-->
             </div>
             <div class="w3-panel w3-border w3-border-red w3-hover-border-green">
-                <p><?php echo anchor('pay2goinvoice/index/touch_invoice', '觸發建立發票', '觸發建立發票') ?></p>
+                <p><?php echo anchor('Pay2GoInvoice/index/touch_invoice', '觸發建立發票', '觸發建立發票') ?></p>
                 <!--<p><a href="index.php/pay2goinvoice/index/touch_invoice">觸發建立發票</a></p>-->
             </div>
             <div class="w3-panel w3-border w3-border-red w3-hover-border-green">
-                <p><?php echo anchor('pay2goinvoice/index/invalid_invoice', '作廢發票', '作廢發票') ?></p>
+                <p><?php echo anchor('Pay2GoInvoice/index/invalid_invoice', '作廢發票', '作廢發票') ?></p>
                 <!--<p><a href="index.php/pay2goinvoice/index/invalid_invoice">作廢發票</a></p>-->
             </div>
             <div class="w3-panel w3-border w3-border-red w3-hover-border-green">
-                <p><?php echo anchor('pay2goinvoice/index/discount_invoice', '發票折讓', '發票折讓') ?></p>
+                <p><?php echo anchor('Pay2GoInvoice/index/discount_invoice', '發票折讓', '發票折讓') ?></p>
                <!--<p><a href="index.php/pay2goinvoice/index/discount_invoice">發票折讓</a></p>-->
             </div>
             <div class="w3-panel w3-border w3-border-red w3-hover-border-green">
-                <p><?php echo anchor('pay2goinvoice/index/query_invoice', '查詢發票', '查詢發票') ?></p>
+                <p><?php echo anchor('Pay2GoInvoice/index/query_invoice', '查詢發票', '查詢發票') ?></p>
                 <!--<p><a href="index.php/pay2goinvoice/index/query_invoice">查詢發票</a></p>-->
             </div>
             <div class="w3-panel w3-border w3-border-red w3-hover-border-green">
-                <p><?php echo anchor('pay2goinvoice/index/query_db', '查詢資料庫', '查詢資料庫') ?></p>
+                <p><?php echo anchor('Pay2GoInvoice/index/query_db', '查詢資料庫', '查詢資料庫') ?></p>
                 <!--<p><a href="index.php/pay2goinvoice/index/query_invoice">查詢發票</a></p>-->
             </div>
 
